86dc26ed9fc3a2b5df981dc37b8742fe64b40fcd
eklaDFF <rahulkrsingh160086@gmail.com>
2025-06-30 00:35:04 +0530

    Add string operation header file

---
+++ 01-c-basics/string-ops/mystring.h
@@ -0,0 +1,11 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+int my_strlen(const char *str);
+int my_strcpy(char *dest, const char *src);
+int my_strcmp(const char *s1, const char *s2);
+void my_strcat(char *dest, const char *src);
+char *my_strchr(const char *str, char c);
+void reverse(char *str);
+
+#endif 
